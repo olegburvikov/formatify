5258365911928378225d498c800a9ef05e4df181
olegburvikov <olegburvikov@gmail.com>
2024-04-07 21:09:43 +0300

    update vite folders structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import {App} from './pages'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const root = document.getElementById('root')!;
+
+const StrictApp = (
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+
+ReactDOM.createRoot(root).render(StrictApp);
